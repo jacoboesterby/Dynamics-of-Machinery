4000ee7d4001c5ec307f8d637ec47eae16f6c532
jacoboesterby <oesterby@live.dk>
2020-03-03 15:11:37 +0100

    Corrected some decoration stuff and included the corresponding natural frequency

--- PlotModeShapes.m
+++ PlotModeShapes.m
@@ -1,4 +1,4 @@
-function PlotModeShapes(phi)
+function PlotModeShapes(phi,omega0)
 
 close all
 syms L
@@ -49,12 +49,13 @@ rhub = 0.05;
 
 wbar = waitbar(0,'Animating...');
 
-ang = zeros(size(phi,1),1);
-phi0 = [0,0,0,0,0,0].';
-f = figure('visible','off');
-hold on
+ang = zeros(size(phi,2),1);
+phi0 = zeros(size(phi,1),1);
+
 
 for i=1:size(phi,2)
+    f = figure('visible','off');
+    hold on
     for k = 1:2
         if k == 1
             sol = phi0;
@@ -70,62 +71,66 @@ for i=1:size(phi,2)
     %Beam and Mass 1
     x1 = q(1)*Phi_beam(Beam(L1),L1);
     y1 = Beam(L1);
-    plot(x1+w,y1,'linewidth',3,'color','k','linestyle',linstyle)
-    plot(x1-w,y1,'linewidth',3,'color','k','linestyle',linstyle)
+    plot(x1+w,y1,'linewidth',3,'color','k','linestyle',linstyle,'HandleVisibility','off')
+    plot(x1-w,y1,'linewidth',3,'color','k','linestyle',linstyle,'HandleVisibility','off')
     
     rectangle('Position',[x1(end)-w,y1(end),w*2,h],'FaceColor',fill,...
-    'LineWidth',3,'linestyle',linstyle)
+    'LineWidth',3,'linestyle',linstyle,'HandleVisibility','off')
 
     %Beam and Mass 2
     x2 = q(2)*Phi_beam(Beam(L2),L2)+x1(end);
     y2 = Beam(L2)+y1(end)+h;
-    plot(x2+w,y2,'linewidth',3,'color','k','linestyle',linstyle)
-    plot(x2-w,y2,'linewidth',3,'color','k','linestyle',linstyle)
+    plot(x2+w,y2,'linewidth',3,'color','k','linestyle',linstyle,'HandleVisibility','off')
+    plot(x2-w,y2,'linewidth',3,'color','k','linestyle',linstyle,'HandleVisibility','off')
     
     rectangle('Position',[x2(end)-w,y2(end),w*2,h],'FaceColor',fill,...
-    'LineWidth',3,'linestyle',linstyle)
+    'LineWidth',3,'linestyle',linstyle,'HandleVisibility','off')
     
     %Beam and Mass 3
     x3 = q(3)*Phi_beam(Beam(L3),L3)+x2(end);
     y3 = Beam(L3)+y2(end)+h;
-    plot(x3+w,y3,'linewidth',3,'color','k','linestyle',linstyle)
-    plot(x3-w,y3,'linewidth',3,'color','k','linestyle',linstyle)
+    plot(x3+w,y3,'linewidth',3,'color','k','linestyle',linstyle,'HandleVisibility','off')
+    plot(x3-w,y3,'linewidth',3,'color','k','linestyle',linstyle,'HandleVisibility','off')
     
     rectangle('Position',[x3(end)-w,y3(end),w*2,h],'FaceColor',fill,...
-    'LineWidth',3,'linestyle',linstyle)
+    'LineWidth',3,'linestyle',linstyle,'HandleVisibility','off')
 
     %Beam and Mass 4
     x4 = q(4)*Phi_beam(Beam(L4),L4)+x3(end);
     y4 = Beam(L4)+y3(end)+h;
-    plot(x4+w,y4,'linewidth',3,'color','k','linestyle',linstyle)
-    plot(x4-w,y4,'linewidth',3,'color','k','linestyle',linstyle)
+    plot(x4+w,y4,'linewidth',3,'color','k','linestyle',linstyle,'HandleVisibility','off')
+    plot(x4-w,y4,'linewidth',3,'color','k','linestyle',linstyle,'HandleVisibility','off')
     
     rectangle('Position',[x4(end)-w,y4(end),w*2,h],'FaceColor',fill,...
-    'LineWidth',3,'linestyle',linstyle)
+    'LineWidth',3,'linestyle',linstyle,'HandleVisibility','off')
     
     %Blades
     % 1
     x5 = (q(5)*Phi_blade(Beam(L5),L5))*cos(ang(i)) +  (Beam(L5))*(-sin(ang(i))) + x4(end) - sin(ang(i))*rhub;
     y5 = (q(5)*Phi_blade(Beam(L5),L5))*sin(ang(i)) +  (Beam(L5))*cos(ang(i)) + y4(end) + h + cos(ang(i))*rhub;
-    plot(x5,y5,'linewidth',3,'color','m','linestyle',linstyle)
-    plot(x5,y5,'linewidth',3,'color','m','linestyle',linstyle)
+    plot(x5,y5,'linewidth',3,'color','m','linestyle',linstyle,'HandleVisibility','off')
+    plot(x5,y5,'linewidth',3,'color','m','linestyle',linstyle,'HandleVisibility','off')
     
     % 2
     x6 = (-q(6)*Phi_blade(Beam(L6),L6))*cos(ang(i)-pi) +  (Beam(L6))*(-sin(ang(i)-pi)) + x4(end) + sin(ang(i))*rhub;
     y6 = (-q(6)*Phi_blade(Beam(L6),L6))*sin(ang(i)-pi) +  (Beam(L6))*cos(ang(i)-pi) + y4(end) + h - cos(ang(i))*rhub;
-    plot(x6,y6,'linewidth',3,'color','m','linestyle',linstyle)
-    plot(x6,y6,'linewidth',3,'color','m','linestyle',linstyle)
+    plot(x6,y6,'linewidth',3,'color','m','linestyle',linstyle,'HandleVisibility','off')
+    plot(x6,y6,'linewidth',3,'color','m','linestyle',linstyle,'HandleVisibility','off')
     
     filledCircle([x4(end),y4(end)+h],rhub,100,'k');
     
-    xlim([-1,1]);
+    %xlim([-1,1]);
     ylim([0,L1+L2+L3+L4+L6+rhub].*1.1)
+    axis('equal')
     
 
     end
+    plot(nan,nan,'k')
+    legend(strcat('$\omega0 = $',sprintf('%.2f Hz',imag(omega0(i)))),'interpreter','latex');
     saveas(f,[pwd,sprintf('/Plots/ModeShape=%.0d.png',i)]);
-    cla;
-    fprintf('Progress: %.2f\n',i/size(phi,1)*100); 
-    waitbar(i/size(phi,1), wbar);
+    close(f);
+    fprintf('Progress: %.2f\n',i/size(phi,2)*100); 
+    waitbar(i/size(phi,2), wbar);
         
 end
+close(wbar);
