1aa6479e209f506072cb5595faf3f7c66049471a
jacoboesterby <oesterby@live.dk>
2020-03-04 14:37:22 +0100

    Stil doesn't work

--- animateFlexibleStructureParFor.m
+++ animateFlexibleStructureParFor.m
@@ -28,17 +28,23 @@ h2 = 0.0015;
 I2 = b2*h2^3/12;
 
 
-Sigma = 0.7341;
-Phi = @(xi,L) cosh(1.87510407/L*(L-xi))+cos(1.87510407/L*(L-xi))-Sigma*(sinh(1.87510407/L*(L-xi))+sin(1.87510407/L*(L-xi)));
-%Define system of equations
-As = diag(ones(1,6).*2);
+Sigma_blade = 0.7341;
+Phi_blade = @(xi,L) cosh(1.87510407/L*(L-xi))+cos(1.87510407/L*(L-xi))-Sigma_blade*(sinh(1.87510407/L*(L-xi))+sin(1.87510407/L*(L-xi)));
+
+Sigma_beam = 0.9825;
+Phi_beam = @(xi,L) cosh(2.36502037/L*(xi))-cos(2.36502037/L*(xi))-Sigma_beam*(sinh(2.36502037/L*(xi))-sin(2.36502037/L*(xi))); 
+%Define system of equations 
+As = diag(ones(1,6));
+As(1:4,:) = As(1:4,:).*double(Phi_beam(1,1));
+As(5:6,:) = As(5:6,:).*double(Phi_blade(1,1));
+
 g = 9.81;
-%%Plotting stuff
-fps = 60;
+%%Plotting stuff 
+fps = 50;
 time = linspace(0,t_int(end),t_int(end)*fps);
 %Create .avi file
 f = figure('visible','off');
-v = VideoWriter('Test1.avi');
+v = VideoWriter('Test3.avi','Framerate',50);
 open(v);
 hold on
 Beam = @(L) linspace(0,L,20);
@@ -48,9 +54,12 @@ ang =@(s) interp1(t_int.',phi.',s);
 w = 0.2;
 h = 0.04;
 rhub = 0.05;
+
+wbar = waitbar(0,'Animating...');
 queue = parallel.pool.DataQueue;
 wq = parallel.pool.DataQueue;
-%afterEach(queue,@updateAnim)
+
+
 afterEach(wq,@UpdateWaitbar)
 afterEach(queue,@updateAnim)
 
@@ -62,7 +71,7 @@ parfor i=1:iter
     hold on
     q = linsolve(As,feval(sol,time(i)).');
     %Beam and Mass 1
-    x1 = q(1)*feval(Phi,feval(Beam,L1),L1);
+    x1 = q(1)*feval(Phi_beam,feval(Beam,L1),L1);
     y1 = feval(Beam,L1);
     plot(x1+w,y1,'linewidth',3,'color','k')
     plot(x1-w,y1,'linewidth',3,'color','k')
@@ -71,7 +80,7 @@ parfor i=1:iter
         'LineWidth',3)
     
     %Beam and Mass 2
-    x2 = q(2)*feval(Phi,feval(Beam,L2),L2)+x1(end);
+    x2 = q(2)*feval(Phi_beam,feval(Beam,L2),L2)+x1(end);
     y2 = feval(Beam,L2)+y1(end)+h;
     plot(x2+w,y2,'linewidth',3,'color','k')
     plot(x2-w,y2,'linewidth',3,'color','k')
@@ -80,7 +89,7 @@ parfor i=1:iter
         'LineWidth',3)
     
     %Beam and Mass 3
-    x3 = q(3)*feval(Phi,feval(Beam,L3),L3)+x2(end);
+    x3 = q(3)*feval(Phi_beam,feval(Beam,L3),L3)+x2(end);
     y3 = feval(Beam,L3)+y2(end)+h;
     plot(x3+w,y3,'linewidth',3,'color','k')
     plot(x3-w,y3,'linewidth',3,'color','k')
@@ -89,7 +98,7 @@ parfor i=1:iter
         'LineWidth',3)
     
     %Beam and Mass 4
-    x4 = q(4)*feval(Phi,feval(Beam,L4),L4)+x3(end);
+    x4 = q(4)*feval(Phi_beam,feval(Beam,L4),L4)+x3(end);
     y4 = feval(Beam,L4)+y3(end)+h;
     plot(x4+w,y4,'linewidth',3,'color','k')
     plot(x4-w,y4,'linewidth',3,'color','k')
@@ -99,36 +108,37 @@ parfor i=1:iter
     
     %Blades
     % 1
-    x5 = (q(5)*feval(Phi,feval(Beam,L5),L5))*cos(feval(ang,time(i))) +  (feval(Beam,L5))*(-sin(feval(ang,time(i)))) + x4(end) - sin(feval(ang,time(i)))*rhub;
-    y5 = (q(5)*feval(Phi,feval(Beam,L5),L5))*sin(feval(ang,time(i))) +  (feval(Beam,L5))*cos(feval(ang,time(i))) + y4(end) + h + cos(feval(ang,time(i)))*rhub;
+    x5 = (q(5)*feval(Phi_blade,feval(Beam,L5),L5))*cos(feval(ang,time(i))) +  (feval(Beam,L5))*(-sin(feval(ang,time(i)))) + x4(end) - sin(feval(ang,time(i)))*rhub;
+    y5 = (q(5)*feval(Phi_blade,feval(Beam,L5),L5))*sin(feval(ang,time(i))) +  (feval(Beam,L5))*cos(feval(ang,time(i))) + y4(end) + h + cos(feval(ang,time(i)))*rhub;
     plot(x5,y5,'linewidth',3,'color','m')
     plot(x5,y5,'linewidth',3,'color','m')
     
     % 2
-    x6 = (-q(6)*feval(Phi,feval(Beam,L6),L6))*cos(feval(ang,time(i))-pi) +  (feval(Beam,L5))*(-sin(feval(ang,time(i))-pi)) + x4(end) + sin(feval(ang,time(i)))*rhub;
-    y6 = (-q(6)*feval(Phi,feval(Beam,L6),L6))*sin(feval(ang,time(i))-pi) +  (feval(Beam,L5))*cos(feval(ang,time(i))-pi) + y4(end) + h - cos(feval(ang,time(i)))*rhub;
+    x6 = (-q(6)*feval(Phi_blade,feval(Beam,L6),L6))*cos(feval(ang,time(i))-pi) +  (feval(Beam,L6))*(-sin(feval(ang,time(i))-pi)) + x4(end) + sin(feval(ang,time(i)))*rhub;
+    y6 = (-q(6)*feval(Phi_blade,feval(Beam,L6),L6))*sin(feval(ang,time(i))-pi) +  (feval(Beam,L6))*cos(feval(ang,time(i))-pi) + y4(end) + h - cos(feval(ang,time(i)))*rhub;
     plot(x6,y6,'linewidth',3,'color','m')
     plot(x6,y6,'linewidth',3,'color','m')
     
     filledCircle([x4(end),y4(end)+h],rhub,100,'k');
     
     xlim([-w*3,w*3]);
-    ylim([0,L1+L2+L3+L4+L6].*1.1)
+    ylim([0,L1+L2+L3+L4+L6+rhub].*1.1)
     frame = getframe(gcf);
-    send(queue,frame)
+    send(queue,{frame,i})
     send(wq,i);
     
     %fprintf('Progress: %.2f\n',i/time(end)*100);
     
     cla;
 end
-
+keyboard
 close(v);
     function UpdateWaitbar(~)
         waitbar(p/iter, wbar);
         p = p+1;
     end
     function updateAnim(s)
-        writeVideo(v,s)
+        s{2}
+        %writeVideo(v,s)
     end
 end
